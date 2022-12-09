532b8682c3e2b3bfeae8019eca909d0ac62a8346
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-12-09 10:31:22 +0800

    feat: publish package xnate-cli

--- packages/xnate-cli/lib/shared/constant.js
+++ packages/xnate-cli/lib/shared/constant.js
@@ -1,4 +1,44 @@
 'use strict';
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (!desc || ('get' in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
+        }
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, 'default', { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o['default'] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null)
+      for (var k in mod)
+        if (k !== 'default' && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+  };
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.SITE_CONFIG =
   exports.SITE_MOBILE_ROUTES =
@@ -35,7 +75,7 @@ exports.SITE_CONFIG =
   exports.NPM_NAME =
   exports.DEFAULT_CLI_HOME =
     void 0;
-var path = require('path');
+var path = __importStar(require('path'));
 exports.DEFAULT_CLI_HOME = '.xnate-cli';
 exports.NPM_NAME = '@xnate-cli/core';
 exports.DEPENDENCIES_PATH = 'dependencies';
